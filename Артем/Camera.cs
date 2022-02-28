1974d237c9e248dd9b0820dd15649eae5e644bd4
Nait <63399368+Nait41@users.noreply.github.com>
2022-02-28 22:39:05 +0300

    Add files via upload

---
+++ Артем/Camera.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompGraphLab1.Scene
+{
+	public class Camera : Transform
+	{
+		private float height;
+		private float length;
+		private float distanceEye;
+		public Camera(float height, float length, float distanceEye)// только примерно определил необходимые величины
+        {
+			this.height = height;
+			this.height = length;
+			this.height = distanceEye;
+		}
+		
+		public float getHeight()
+        {
+			return height;
+        }
+
+		public float getLength()
+		{
+			return length;
+		}
+
+		public float getDistanceEye()
+		{
+			return distanceEye;
+		}
+	}
+}
---
+++ Артем/MeshProjector.cs
@@ -0,0 +1,29 @@
+﻿using CompGraphLab1.Data;
+using CompGraphLab1.Scene;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompGraphLab1.Rendering
+{
+	class MeshProjector : IMeshProjector
+	{
+		public ObjPlanaredData placePoint(ObjData mesh, Camera camera)// делал по статье
+        {
+			int countTris = 0;
+			int countVerts = 0;
+			ObjPlanaredData planarData = new ObjPlanaredData();// нормализованные координаты
+			foreach(Triangle3D triangle3D in mesh.tris)
+			{
+				foreach(Vector3 vect3 in triangle3D.verts)// использовал формулы, указанные во 2 главе
+				{
+					planarData.tris[countTris].verts[countVerts].x = (float)((vect3.x / -vect3.z) + camera.getLength() * 0.5) / camera.getLength();
+					planarData.tris[countTris].verts[countVerts].y = (float)((vect3.y / -vect3.z) + camera.getHeight() * 0.5) / camera.getHeight();
+					countVerts++;
+				}
+				countTris++;
+			}
+			return planarData;
+		}
+	}
+}
